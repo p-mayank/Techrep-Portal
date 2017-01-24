afbdb421de9ed1391e3e78ecd338de0e731815e7
Mayank Patidar <er.mayank96@gmail.com>
2017-01-25 01:13:29 +0530

    Refined.

--- index.php
+++ index.php
@@ -78,14 +78,6 @@ $_SESSION['time']     = time();
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2 text">
                             <a href="http://www.techniche.org" target="_blank"><img src="img.png"></a>
-
-                            <!--<h1><strong>Bootstrap</strong> Login Form</h1>-->
-                            <div class="description">
-                            	<!--<p>
-	                            	This is a free responsive login form made with Bootstrap. 
-	                            	Download it on <a href="http://azmind.com"><strong>AZMIND</strong></a>, customize and use it as you like!
-                            	</p>-->
-                            </div>
                         </div>
                     </div>
                     <div class="row">
@@ -124,33 +116,11 @@ HTML;
 			                    		<label class="sr-only" for="form-username">EmailID</label>
 			                        	<input type="text" name="form-username" placeholder="Username..." class="form-username form-control" id="form-username">
 			                        </div>
-                                    <!--
-			                        <div class="form-group">
-			                        	<label class="sr-only" for="form-password">Password</label>
-			                        	<input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
-			                        </div>-->
 			                        <button type="submit" class="btn">Sign in!</button>
 			                    </form>
 		                    </div>
                         </div>
                     </div>
-                    <!--
-                    <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3 social-login">
-                        	<h3>...or login with:</h3>
-                        	<div class="social-login-buttons">
-	                        	<a class="btn btn-link-2" href="#">
-	                        		<i class="fa fa-facebook"></i> Facebook
-	                        	</a>
-	                        	<a class="btn btn-link-2" href="#">
-	                        		<i class="fa fa-twitter"></i> Twitter
-	                        	</a>
-	                        	<a class="btn btn-link-2" href="#">
-	                        		<i class="fa fa-google-plus"></i> Google Plus
-	                        	</a>
-                        	</div>
-                        </div>
-                    </div>-->
                 </div>
             </div>
             
@@ -163,9 +133,6 @@ HTML;
         <script src="assets/js/jquery.backstretch.min.js"></script>
         <script src="assets/js/scripts.js"></script>
         
-        <!--[if lt IE 10]>
-            <script src="assets/js/placeholder.js"></script>
-        <![endif]-->
 
     </body>
 
